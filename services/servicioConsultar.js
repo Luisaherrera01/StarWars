df6d5c70091914d2819f2ceb06f0899bdded2a5e
Luisaherrera01 <helu1020@gmail.com>
2023-05-10 14:47:20 -0500

    se crean las cards de las especies

--- services/servicioConsultar.js
+++ services/servicioConsultar.js
@@ -9,3 +9,15 @@ export async function consultarPlanetas(){
     return planetasEntregados
 
 }
+
+export async function consultarEspecies(){
+    const URL = "https://swapi.dev/api/species/"
+
+    const PETICION = {
+        method: "GET"
+    }
+    let respuesta1=await fetch(URL,PETICION)
+    let especiesEntregados=await respuesta1.json()
+    return especiesEntregados
+
+}
--- src/controlador.js
+++ src/controlador.js
@@ -1,4 +1,6 @@
 import { consultarPlanetas } from "../services/servicioConsultar.js"
+import { consultarEspecies } from "../services/servicioConsultar.js"
+
 
 let fila = document.querySelector("#fila")
 
@@ -32,6 +34,44 @@ consultarPlanetas()
             tarjeta.appendChild(clima)
             columna.appendChild(tarjeta)
             fila.appendChild(columna)
+    })
+
+})
+
+
+
+let fila1 = document.querySelector("#fila1")
+
+consultarEspecies()
+.then(function(respuesta1){
+    console.log(respuesta1)
+
+   respuesta1.results.forEach(function(especie){
+
+        let columna = document.createElement("div")
+        columna.classList.add("col","mb-3")
+
+        let tarjeta = document.createElement("div")
+        tarjeta.classList.add("card","h-100","shadow")
+
+        let nombre = document.createElement("h4")
+        nombre.classList.add("text-center", "fw-bold")
+        nombre.textContent=especie.name
+
+        let skin_colors = document.createElement("p")
+        skin_colors.classList.add("text-right","p-2", "pb-0")
+        skin_colors.innerHTML = "<strong>Color de piel: </strong>"+ especie.skin_colors
+
+        let hair_colors = document.createElement("p")
+        hair_colors.classList.add("text-right","p-2", "pb-0")
+        hair_colors.innerHTML = "<strong>Color de cabello: </strong>" + especie.hair_colors
+        
+
+            tarjeta.appendChild(nombre)
+            tarjeta.appendChild(skin_colors)
+            tarjeta.appendChild(hair_colors)
+            columna.appendChild(tarjeta)
+            fila1.appendChild(columna)
     })
 
 })
